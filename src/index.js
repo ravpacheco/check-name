b497c074d15eeb24301def3b6bd3156763b5aa68
Rafael Pacheco <ravpacheco@gmail.com>
2019-03-09 16:41:30 -0300

    Add functions to handle requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 var express = require('express');
+var fs = require('fs');
+var parse = require('csv-parse');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(bodyParser.json()); // for parsing application/json
+
+var namesDb = {};
+
+app.get('/verify/:name', function (req, res) {
+
+    let name = req.params.name;
+    console.log(name);
     res.send({ 'hi': 'hello world' });
 });
+
+app.post('/verify', function (req, res) {
+
+    console.log(req.body);
+    let name = req.body.name;
+    console.log(name);
+    res.send('Got a POST request')
+  })
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log(`Express started on port ${port}`);
-});
 
+    var parser = parse({ delimiter: ',' }, function (err, data) {
+        
+        data.forEach(element => {
+            namesDb[element[0]] = element;   
+        })
+        console.log(namesDb['Nome']);
+    });
+
+    fs.createReadStream(__dirname + '/ibge2010-names.csv').pipe(parser);
+});
