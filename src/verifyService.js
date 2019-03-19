b7822a0a6fd4ea1f3ae0809d8292aef98084ba30
Rafael Pacheco <ravpacheco@gmail.com>
2019-03-18 23:42:41 -0300

    Add support for multiple languages and add en-us names

--- src/verifyService.js
+++ src/verifyService.js
@@ -29,14 +29,49 @@ module.exports = {
             namesFound: namesFound
         }
     },
-    init: function () {
+    parseCSVToJSON: function () {
         var parser = parse({ delimiter: ',' }, function (err, data) {
 
+            var _namesDb = {};
             data.forEach(element => {
-                namesDb[element[0]] = element;
+                _namesDb[element[0].toUpperCase()] = element;
             })
+
+            fs.exists('parsed-names.json', function (exists) {
+                if (exists) {
+                    console.log("parsed-names file exists");
+                    fs.readFile('parsed-names.json', function readFileCallback(err, data) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            var json = JSON.stringify(_namesDb);
+                            fs.writeFile('parsed-names.json', json);
+                        }
+                    });
+                } else {
+                    console.log("file not exists")
+                    var json = JSON.stringify(_namesDb, null, '\t');
+                    fs.writeFile('parsed-names.json', json, (e) => console.log(e));
+                }
+            });
         });
 
-        fs.createReadStream(__dirname + '/ibge2010-names.csv').pipe(parser);
+        // fs.createReadStream(__dirname + '/data/pt-br/ibge2010-names.csv').pipe(parser);
+        // fs.createReadStream(__dirname + '/data/en-us/social-security-names.csv').pipe(parser);
+        // fs.createReadStream(__dirname + '/data/custom-names.csv').pipe(parser);
+    },
+    init: function () {
+
+        let supportedLangs = [
+            'en-us',
+            'pt-br',
+            '../data' // For custom names
+        ];
+
+        for (var i = 0, len = supportedLangs.length; i < len; i++) {
+            let lang = supportedLangs[i];
+            let rawdata = fs.readFileSync(__dirname + `/data/${lang}/names.json`);
+            namesDb = { ...namesDb, ...JSON.parse(rawdata) };
+        }
     }
 };
